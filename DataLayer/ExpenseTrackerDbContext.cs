95201d493a81bbe4276a010a3671cef28e3d7555
shelton <sheltoncarvallo975@gmail.com>
2024-11-02 18:46:41 -0500

    Addind tables to the DbContext

--- DataLayer/ExpenseTrackerDbContext.cs
+++ DataLayer/ExpenseTrackerDbContext.cs
@@ -1,8 +1,27 @@
 using Microsoft.EntityFrameworkCore;
+using ModelLayer.Models;
 
 namespace DataLayer;
 
 public class ExpenseTrackerDbContext : DbContext
 {
-    
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public ExpenseTrackerDbContext(DbContextOptions<ExpenseTrackerDbContext> options) : base(options)
+    {
+    }
+
+    public ExpenseTrackerDbContext()
+    {
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(
+            "data source=DESKTOP-REDKEOB;initial catalog=ExpenseTrackerDevelopment;user id= PortalApi;password=123456;TrustServerCertificate=True;MultipleActiveResultSets=true");
+
+    public DbSet<Balance> Balances => Set<Balance>();
+    public DbSet<Bank> Banks => Set<Bank>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<PaymentMethod> PaymentMethods => Set<PaymentMethod>();
+    public DbSet<Status> Statuses => Set<Status>();
+    public DbSet<User> Users => Set<User>();
 }
--- ModelLayer/Models/Status.cs
+++ ModelLayer/Models/Status.cs
@@ -6,6 +6,6 @@ public class Status
    public string StatusDescription { get; set; } = string.Empty;
    //Navigation Properties are used to reference the related entity 
    //It can be defined on the principal or dependent entity
-   public ICollection<User> Users { get; set; } = new List<User>(); //Collection navigation property
+   //public ICollection<User> Users { get; set; } = new List<User>(); //Collection navigation property
    
 } 
